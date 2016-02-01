1ed09e9ba3d401a4ab46088830d7d5426f17552e
Johnathan Evans <joe.joevans@gmail.com>
2016-02-01 13:57:19 -0700

    updated comments

--- onScroll.js
+++ onScroll.js
@@ -1,13 +1,17 @@
+<script type="text/javascript">
 jQuery(function($){
 	$(document).scroll(function(){
-		/* examples of scroll function; onScroll(element, minWindowScroll, maxWindowScroll) */
+		/* examples of scroll function
+		** onScroll(element, (int)minWindowScroll, (int)maxWindowScroll); */
 		onScroll($('.portfolio-section-1'), 850, 1000);
 		onScroll($('.portfolio-section-1 h2'), 1000, 1300);
 	});
 
 	function onScroll(findSection, winMin, winMax){
 		if(findSection && winMin && winMax){
-			// once vars are passed in, the function will see what the scrollTop is and calculate max and min of passed in vars.
+			// Main Function
+			// Once vars are passed in, the function will see what the
+			// scrollTop is and calculate max and min of passed in vars.
 			var winScoll = $(window).scrollTop();
 			var dif = (winScoll-winMin)/100;
 			if(winScoll < winMin){
@@ -24,3 +28,5 @@ jQuery(function($){
 		}
 	}
 });
+
+</script>
